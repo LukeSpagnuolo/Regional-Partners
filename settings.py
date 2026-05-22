e423d6ebec64dc747b6cc71c5f81532d59d05df5
Luke Spagnuolo <lukespagnuolo@uvic.ca>
2026-05-22 09:50:50 -0700

    Add environment variable placeholders and new API endpoints
    Added environment variable placeholders for CLIENT_ID, CLIENT_SECRET, and updated API endpoints.

--- settings.py
+++ settings.py
@@ -2,15 +2,27 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+"""
+.env example
+
+CLIENT_ID=<from oauth>
+CLIENT_SECRET=<from oauth>
+
+SITE_URL=http://<address>:8000
+APP_URL=http://<address>:8050
+
+"""
+
 SITE_URL = os.environ.get("SITE_URL","http://127.0.0.1:8000")
 APP_URL = os.environ.get("APP_URL","http://127.0.0.1:8050")
 
-
-# using spotify as an example
 AUTH_URL = f"{SITE_URL}/o/authorize"
 TOKEN_URL = f"{SITE_URL}/o/token/"
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
-API_PEOPLE_URL = f"{SITE_URL}/api/registration/profile/"
-API_ME_URL = f"{SITE_URL}/api/csiauth/me/"
+SPORT_ORG_ENDPOINT = f"/api/registration/organization/"
+PROFILE_ENDPOINT = f"/api/registration/profile/"
+
+REPORT_COLUMNS_ENDPOINT = "/api/registration/report-columns/"
+REPORT_ROWS_ENDPOINT = "/api/registration/report-rows/"
